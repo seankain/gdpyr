faba9ad06e3fcc344146451b347f1c8ec0b370eb
unknown <sean.kain.geo@gmail.com>
2024-08-25 21:48:56 -0400

    using csgboxes for level prototyping, added some prototyping textures from itch

---
+++ PlayerController/PlayerSlidingState.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class PlayerSlidingState : State
+{
+	[Export]
+	public float TiltAmount = 0.09f;
+	[Export]
+	public float SlideAnimSpeed = 4.0f;
+
+	[Export]
+	public ShapeCast3D CrouchShapeCast;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	public override void Enter()
+	{
+		base.Enter();
+	}
+	public override void Update(double delta)
+	{
+		//base.Update(delta);
+		//Not doing base to avoid updating input so player cant change direction while sliding
+		playerController.UpdateGravity(delta);
+		playerController.UpdateVelocity();
+	}
+
+	public void SetTilt(float player_rotation)
+	{
+
+	}
+
+}
